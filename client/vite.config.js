259e3fd6dfbc5909dbfcafa1bec831414b646629
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-02-09 00:00:10 +0800

    proxy variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://chat-app-xsxm.onrender.com',
         changeOrigin: true,
         secure: false,
         ws: true,
